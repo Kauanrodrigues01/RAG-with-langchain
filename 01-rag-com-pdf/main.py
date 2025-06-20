ce3f732ebd237688b90ad475303eaa8f15daa4fc
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-06-20 16:44:51 -0300

    feat(rag): add RAG example using PDF as data source

---
+++ 01-rag-com-pdf/main.py
@@ -0,0 +1,59 @@
+import os
+from pathlib import Path
+from decouple import config
+from langchain import hub
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+
+OPENAI_API_KEY = config('OPENAI_API_KEY')
+os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+
+llm = ChatOpenAI(
+    model='gpt-3.5-turbo',
+    temperature=0.0,
+    max_tokens=1000,
+)
+
+pdf_path = Path(__file__).parent / 'laptop_manual.pdf'
+
+pdf_loader = PyPDFLoader(file_path=pdf_path)
+
+docs = pdf_loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+)
+chunks = text_splitter.split_documents(documents=docs)
+
+embedding = OpenAIEmbeddings(model='text-embedding-3-small')
+
+vector_store = Chroma.from_documents(
+    documents=chunks,
+    embedding=embedding,
+    collection_name='laptop_manual',
+)
+
+retriever = vector_store.as_retriever()
+
+instructions = hub.pull('rlm/rag-prompt')
+
+rag_chain = (
+    {
+        'context': retriever,
+        'question': RunnablePassthrough(),  # Pede para o usuário inserir a pergunta
+    }
+    | instructions
+    | llm
+    | StrOutputParser()
+)
+
+question = 'Como funciona o cancelamento de ruido inteligente?'
+
+response = rag_chain.invoke(question)
+
+print(response)
